b5e5557cba8bec18ed92cdd6c17423ffdf9a792f
PonyCui <cuiminghui@yy.com>
2015-09-08 17:12:19 +0800

    The author is Pony

--- PonyChatUI/Classes/User Interface/View/PCUSlideUpCell.h
+++ PonyChatUI/Classes/User Interface/View/PCUSlideUpCell.h
@@ -3,7 +3,7 @@
 //  xiaoquan
 //
 //  Created by 崔 明辉 on 15/9/6.
-//  Copyright (c) 2015年 Alex. All rights reserved.
+//  Copyright (c) 2015年 Pony.Cui. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
