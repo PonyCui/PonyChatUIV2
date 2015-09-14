14e57838c54369a6df360c4060287be3f421a91a
ponycui <PonyCui_Home@saitekiiMac.local>
2015-09-14 23:44:07 +0800

    add image loading cache

--- PonyChatUI/Classes/Applicaton Logic/Manager/PCUImageManager.m
+++ PonyChatUI/Classes/Applicaton Logic/Manager/PCUImageManager.m
@@ -7,6 +7,7 @@
 //
 
 #import "PCUImageManager.h"
+#import <CommonCrypto/CommonCrypto.h>
 
 @interface PCUImageManager ()
 
@@ -38,6 +39,9 @@
 
 - (void)fetchCachedImageWithURL:(NSURL *)URL callbackQueue:(dispatch_queue_t)callbackQueue completion:(void (^)(CGImageRef))completion {
     NSData *data = [self.avatarCacheObject valueForKey:URL.absoluteString];
+    if (data == nil) {
+        data = [NSData dataWithContentsOfFile:[self cachePathWithURL:URL]];
+    }
     if (data != nil) {
         CGImageRef imageRef = [[UIImage imageWithData:data] CGImage];
         CFRetain(imageRef);
@@ -63,7 +67,7 @@
 - (id)downloadImageWithURL:(NSURL *)URL callbackQueue:(dispatch_queue_t)callbackQueue downloadProgressBlock:(void (^)(CGFloat))downloadProgressBlock completion:(void (^)(CGImageRef, NSError *))completion {
     NSURLRequest *request = [NSURLRequest requestWithURL:URL
                                              cachePolicy:NSURLRequestReturnCacheDataElseLoad
-                                         timeoutInterval:15.0];
+                                         timeoutInterval:60.0];
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
         if (connectionError == nil && [data isKindOfClass:[NSData class]]) {
             CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData((__bridge CFDataRef)(data));
@@ -75,6 +79,9 @@
             if (imageRef != nil) {
                 if ([data length] < 1024 * 64) {
                     [self.avatarCacheObject setValue:data forKey:URL.absoluteString];
+                }
+                else {
+                    [data writeToFile:[self cachePathWithURL:URL] atomically:NO];
                 }
                 dispatch_async(callbackQueue, ^{
                     completion(imageRef, nil);
@@ -98,6 +105,28 @@
 }
 
 - (void)cancelImageDownloadForIdentifier:(id)downloadIdentifier {
+    
+}
+
+- (NSString *)cachePathWithURL:(NSURL *)URL {
+    
+    CC_MD5_CTX md5;
+    CC_MD5_Init (&md5);
+    CC_MD5_Update (&md5, [[URL absoluteString] UTF8String], (CC_LONG) [[URL absoluteString] length]);
+    
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5_Final (digest, &md5);
+    NSString *s = [NSString stringWithFormat: @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+                   digest[0],  digest[1],
+                   digest[2],  digest[3],
+                   digest[4],  digest[5],
+                   digest[6],  digest[7],
+                   digest[8],  digest[9],
+                   digest[10], digest[11],
+                   digest[12], digest[13],
+                   digest[14], digest[15]];
+    
+    return [NSString stringWithFormat:@"%@/%@", NSTemporaryDirectory(), s];
     
 }
 
